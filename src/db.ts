d49ba7ffbb8c7aebad902f891dfe78d400699c28
phuown <pthuphuong051103@gmail.com>
2025-10-11 20:57:31 +0700

    db bang neon

--- src/db.ts
+++ src/db.ts
@@ -1,15 +1,10 @@
 import { Pool } from "pg";
-
 const pool = new Pool({
   user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,        // ==> đổi sang host Neon
+  database: process.env.DB_NAME,    // ==> đổi sang tên DB Neon
   password: process.env.DB_PASSWORD,
   port: Number(process.env.DB_PORT) || 5432,
+  ssl: { rejectUnauthorized: true }, // hoặc thêm ?sslmode=require trong URL
 });
-
-pool.connect()
-  .then(() => console.log("PostgreSQL kết nối thành công "))
-  .catch((err) => console.error("PostgreSQL kết nối không thành công:", err));
-
 export default pool;
