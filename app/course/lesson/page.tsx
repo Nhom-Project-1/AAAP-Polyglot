bbdc090e955e2746cc4c8c713ccd839d312ad527
Nguyen Phuong Anh <nphuonganh14022004@gmail.com>
2025-10-14 15:55:09 +0700

    Giao dien lesson hien thi list vocab va nut challenge

--- app/course/lesson/page.tsx
+++ app/course/lesson/page.tsx
@@ -162,6 +162,7 @@ export default function LessonPage() {
     const foundUnit = allUnits.find(u => u.id === unitId) || null
     return { lesson: foundLesson, unit: foundUnit }
   }, [lessonId, unitId])
+  const lessonVocabs = mockVocabs.filter(v => v.ma_bai_hoc === lesson?.id)
 
 if (!lesson || !unit) 
   return(
@@ -201,23 +202,31 @@ if (!lesson || !unit)
       <div className="text-center mt-8">
         <p className="text-3xl font-semibold">Danh sách từ vựng</p>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-6">
-        {mockVocabs.map((v) => (
-          <div key={v.ma_tu} className="border rounded-xl p-4 shadow-md flex items-center justify-between">
-            <div>
-              <h3 className="text-xl font-bold">{v.tu} <span className="text-gray-500">{v.phien_am}</span></h3>
-              <p className="text-gray-700 mb-2">{v.nghia}</p>
-              <p className="italic text-gray-500">{v.vi_du}</p>
-            </div>
-            <button
-              onClick={() => playAudio(v.lien_ket_am_thanh)}
-              className="ml-4 text-pink-500 hover:text-pink-600 cursor-pointer"
-            >
-              <Volume2 size={24} />
-            </button>
-          </div>
-        ))}
+      
+     <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-6">
+  {lessonVocabs.length > 0 ? (
+    lessonVocabs.map((v) => (
+      <div key={v.ma_tu} className="border rounded-xl p-4 shadow-md flex items-center justify-between">
+        <div>
+          <h3 className="text-xl font-bold">{v.tu} <span className="text-gray-500">{v.phien_am}</span></h3>
+          <p className="text-gray-700 mb-2">{v.nghia}</p>
+          <p className="italic text-gray-500">{v.vi_du}</p>
+        </div>
+        <button
+          onClick={() => playAudio(v.lien_ket_am_thanh)}
+          className="ml-4 text-pink-500 hover:text-pink-600 cursor-pointer"
+        >
+          <Volume2 size={24} />
+        </button>
       </div>
+    ))
+  ) : (
+    <p className="col-span-full text-gray-500 text-center italic">
+      Hiện tại chưa có từ vựng cho bài này
+    </p>
+  )}
+</div>
+
        <div className="flex justify-center mt-12 mb-6">
         <motion.button
           whileHover={{ scale: 1.05 }}
