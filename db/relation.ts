b28d4a12a925c86062939185e0ac4b551e6972ee
phuown <pthuphuong051103@gmail.com>
2025-10-29 15:05:17 +0700

    Bang quan he

--- db/relation.ts
+++ db/relation.ts
@@ -1,5 +1,5 @@
 import { relations } from "drizzle-orm";
-import { unit, bai_hoc, tu_vung, ngon_ngu } from "./schema";
+import { unit, bai_hoc, tu_vung, ngon_ngu, thu_thach, lua_chon_thu_thach } from "./schema";
 
 export const unitRelations = relations(unit, ({ one, many }) => ({
   ngonNgu: one(ngon_ngu, {
@@ -21,5 +21,20 @@ export const tuVungRelations = relations(tu_vung, ({ one }) => ({
   baiHoc: one(bai_hoc, {
     fields: [tu_vung.ma_bai_hoc],
     references: [bai_hoc.ma_bai_hoc],
+  }),
+}));
+
+export const thuThachRelations = relations(thu_thach, ({ many, one }) => ({
+  lua_chon_thu_thach: many(lua_chon_thu_thach),
+  bai_hoc: one(bai_hoc, {
+    fields: [thu_thach.ma_bai_hoc],
+    references: [bai_hoc.ma_bai_hoc],
+  }),
+}));
+
+export const luaChonThuThachRelations = relations(lua_chon_thu_thach, ({ one }) => ({
+  thu_thach: one(thu_thach, {
+    fields: [lua_chon_thu_thach.ma_thu_thach],
+    references: [thu_thach.ma_thu_thach],
   }),
 }));
