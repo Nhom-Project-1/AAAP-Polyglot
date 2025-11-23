31d7df61a21bf591e728c73905bd78f6646cb418
Nguyễn Tú Anh <anh.nt.004@gmail.com>
2025-11-24 00:37:00 +0700

    fix build github action

--- db/drizzle.ts
+++ db/drizzle.ts
@@ -4,8 +4,14 @@ import { neon } from "@neondatabase/serverless";
 import * as schema from "./schema";
 import * as relations from "./relation";
 
-const sql = neon(process.env.DATABASE_URL || ""); 
-export const db = drizzle(sql, {schema : { ...schema, ...relations}});
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not defined");
+}
+if (process.env.DATABASE_URL === "<YOUR_NEON_DATABASE_URL_HERE>") {
+  throw new Error("DATABASE_URL is set to a placeholder. Please set the DATABASE_URL environment variable in your GitHub Secrets.");
+}
+const sql = neon(process.env.DATABASE_URL);
+export const db = drizzle(sql, { schema: { ...schema, ...relations } });
 
-export { schema }; 
+export { schema };
 export default db;
